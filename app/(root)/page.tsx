4ae27ea93a7a5dba22d84ac94ce1157048fdc6cf
abdulrahmanRadan <aboda123li123@gmail.com>
2024-12-01 16:20:21 +0300

    Update some code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,27 @@
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Abdulrahman" };
+
   return (
     <section className="home">
-      <div className="home-content">Home and header </div>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Radan"}
+            subtext="Access and manage your account and transactions efficiently. "
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
     </section>
   );
 };
