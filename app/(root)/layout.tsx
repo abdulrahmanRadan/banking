ee176327e822814401d42f1f7ca7724f88abe820
abdulrahmanRadan <aboda123li123@gmail.com>
2024-12-01 17:41:14 +0300

    Add Sidebar to main page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Radan", lastName: "JSM" };
   return (
-    <main>
-      SIDEPAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
