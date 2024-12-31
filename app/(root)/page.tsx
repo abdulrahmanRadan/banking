0a205f9ab0bb24a4a583d9c744f44de12d36c290
abdulrahmanRadan <aboda123li123@gmail.com>
2024-12-31 17:01:23 +0300

    add bank card component and add in it the catd content :account name and account current balance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,7 +28,11 @@ const Home = () => {
         </header>
         RECENT TRANSACTIONS
       </div>
-      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
